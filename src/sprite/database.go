7548a4c898459f92bececba6ab0c01a6211ac1f9
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-02-27 15:02:26 -0500

    clean database file

--- src/sprite/database.go
+++ src/sprite/database.go
@@ -14,37 +14,39 @@ import (
 )
 
 func Database(input string, output string) {
-	fmt.Printf("Sprite Database Function Executing\n Converting Sprites in %s and saving to output %s", input, output)
+	fmt.Println("Building database ...")
+	fmt.Println("Converting sprites in %s and saving to output %s", input, output)
+
 	spriteColorDatabase := make(map[string]util.Rgb)
 
 	sprites, err := os.ReadDir(input)
 	if err != nil {
-		log.Fatal(err) //fatal might be a bit much if you make this into a cmd exe
+		log.Fatal(err)
 	}
-	for _, entry := range sprites { //study this syntax
-		if !entry.IsDir() { //TODO maybe need a check that it's a PNG file?
+	for _, entry := range sprites {
+		if !entry.IsDir() {
+			if filepath.Ext(entry.Name()) != ".png" {
+				continue
+			}
 			filePath := filepath.Join(input, entry.Name())
 			spriteColorDatabase[entry.Name()] = averageColor(filePath)
 		}
 	}
 
-	//Save Database file
 	file, err := os.Create(filepath.Join(util.DatabaseFolderPath, "sprite_color_db"))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer file.Close()
+
 	encoder := gob.NewEncoder(file)
 	if err := encoder.Encode(spriteColorDatabase); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
-//avereage color, local function
-
 func averageColor(imagePath string) util.Rgb {
-
 	file, err := os.Open(imagePath)
 	if err != nil {
 		log.Fatal(err)
@@ -58,20 +60,23 @@ func averageColor(imagePath string) util.Rgb {
 
 	rTotal, gTotal, bTotal := 0, 0, 0
 	bounds := img.Bounds()
-	pixelTotal := bounds.Dx() * bounds.Dy() //total amount of pixels
+	pixelTotal := bounds.Dx() * bounds.Dy()
 
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			rgbcolor := util.GetRGB(x, y, img) //should work but test
+			rgbcolor := util.GetRGB(x, y, img)
 			rTotal += int(rgbcolor.R)
 			gTotal += int(rgbcolor.G)
 			bTotal += int(rgbcolor.B)
 		}
 	}
+
 	var average util.Rgb
 	average.R = int(rTotal / pixelTotal)
 	average.G = int(gTotal / pixelTotal)
 	average.B = int(bTotal / pixelTotal)
+
 	fmt.Printf("average color is: %d\n", average)
+
 	return average
 }
