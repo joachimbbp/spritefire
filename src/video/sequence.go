4a1f28f4bfb9e8a0543bc444d0f814ff80187ee2
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-02-27 15:35:54 -0500

    refactored sequence

--- src/video/sequence.go
+++ src/video/sequence.go
@@ -9,20 +9,20 @@ import (
 	"github.com/joachimbbp/spritefire/src/util"
 )
 
-// takes in a sequence
 func Sequence(sequencePath string, spriteColorDbPath string, spriteSizeIndex int) {
-	//for every image in the sequence path run:
-	//mosaic.Draw(mosaic.Canvas(util.InputStill, util.DatabasePath, util.ResizeResolutions[sRes])
 	frames, err := os.ReadDir(sequencePath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, frame := range frames {
 		if filepath.Ext(frame.Name()) != ".png" {
 			continue
 		}
+
 		framePath := filepath.Join(sequencePath, frame.Name())
-		mosaic.Draw(mosaic.Canvas(framePath, util.DatabasePath, util.ResizeResolutions[spriteSizeIndex]), frame.Name(), util.ResizeResolutions[spriteSizeIndex])
+		canvas := mosaic.Canvas(framePath, util.DatabasePath, util.ResizeResolutions[spriteSizeIndex])
+		mosaic.Draw(canvas, frame.Name(), util.ResizeResolutions[spriteSizeIndex])
 	}
 
 }
