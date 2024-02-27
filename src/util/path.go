015bbb5c64633af408db84de20ae780ea4c21ec6
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-02-27 15:34:05 -0500

    relative paths

--- src/util/path.go
+++ src/util/path.go
@@ -1,17 +1,15 @@
 package util
 
-//Perhaps once this becomes dynamic
-//you could do a path file and rename accordingly:
-//path.Database, path.InputStill, etc
+const BasePath = "../../assets"
 
-// Temp paths for testing and development
-const DatabaseFolderPath = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/database"
-const SpriteInput = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/sprites_512"
-const SpriteSizes = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/sprite_sizes"
-const InputStill = "/Users/joachimpfefferkorn/Documents/GitHub/sprite_mosaic/test_files/test_images/garden_1280x720.png"
-const ScratchOutput = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/scratch"
-const CanvasOutput = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/canvas"
-const DatabasePath = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/database/sprite_color_db"
-const SequencePath = "/Users/joachimpfefferkorn/Documents/GitHub/spritefire/ignore/test_images/GodzillaVsHedorah"
+const DatabaseFolderPath = BasePath + "/database"
+const DatabasePath = BasePath + "/database/sprite_color_db"
 
-//hacky for now, but it's just for testing
+const SpriteInput = BasePath + "/sprites_512"
+const SpriteSizes = BasePath + "/sprite_sizes"
+const ScratchOutput = BasePath + "/scratch"
+
+// const CanvasOutput = BasePath + "/canvas"
+
+const InputStill = BasePath + "/test_images/garden_1280x720.png"
+const SequencePath = BasePath + "/test_images/GodzillaVsHedorah"
