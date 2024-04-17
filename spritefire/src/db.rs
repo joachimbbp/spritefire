f8abb35967e656879038fb4b12831580a3daaae2
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2024-04-17 19:23:20 -0400

    Add option to debug avg color for emoji

--- spritefire/src/db.rs
+++ spritefire/src/db.rs
@@ -1,5 +1,6 @@
 use std::{
     fs,
+    io::Write,
     path::{Path, PathBuf},
 };
 
@@ -48,8 +49,6 @@ impl EmojiDatabase {
             kdtree.add(&color, i as u32);
         });
 
-        // dbg!(&symbols);
-        println!("{}", kdtree.size());
         Self { symbols, kdtree }
     }
 
@@ -64,51 +63,8 @@ impl EmojiDatabase {
         let index = nearest.item as usize;
         &self.symbols[index]
     }
-
-    pub fn from_directory(dir_path: PathBuf) -> Self {
-        let dir_path = Path::new(&dir_path);
-        let entries = match fs::read_dir(dir_path) {
-            Ok(val) => val,
-            Err(err) => {
-                panic!("Error: {}", err);
-            }
-        };
-
-        let mut emojis: Vec<Emoji> = Vec::new();
-        for entry in entries {
-            let entry = match entry {
-                Ok(val) => val,
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                    continue;
-                }
-            };
-
-            let file_path = entry.path();
-            let file_stem = file_path.file_stem().unwrap();
-
-            if let Some(file_name_str) = file_stem.to_str() {
-                let parts: Vec<&str> = file_name_str.split('_').collect();
-                let mut parts = parts.iter();
-
-                let mut emoji_unicode_point = String::new();
-                let _ = parts.next().unwrap();
-                let first = parts.next().unwrap().strip_prefix("u").unwrap();
-                let code_point = u32::from_str_radix(first, 16).unwrap();
-                let code_point = char::from_u32(code_point).unwrap().to_string();
-                emoji_unicode_point.push_str(&code_point);
-
-                while let Some(part) = parts.next() {
-                    let code_point = u32::from_str_radix(part, 16).unwrap();
-                    let code_point = char::from_u32(code_point).unwrap().to_string();
-                    emoji_unicode_point.push_str(&code_point);
-                }
-
-                let img = image::open(&file_path).unwrap();
-                emojis.push(Emoji::from((img, emoji_unicode_point)));
-            }
-        }
-
+    pub fn new_from_directory(dir_path: PathBuf) -> Self {
+        let emojis = read_emojis_from_directory(dir_path);
         EmojiDatabase::from_emojis(emojis)
     }
 
@@ -159,4 +115,73 @@ impl EmojiDatabase {
 
         emojis
     }
+}
+
+pub fn read_emojis_from_directory(dir_path: PathBuf) -> Vec<Emoji> {
+    let dir_path = Path::new(&dir_path);
+    let entries = match fs::read_dir(dir_path) {
+        Ok(val) => val,
+        Err(err) => {
+            panic!("Error: {}", err);
+        }
+    };
+
+    let mut emojis: Vec<Emoji> = Vec::new();
+    for entry in entries {
+        let entry = match entry {
+            Ok(val) => val,
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                continue;
+            }
+        };
+
+        let file_path = entry.path();
+        let file_stem = file_path.file_stem().unwrap();
+
+        if let Some(file_name_str) = file_stem.to_str() {
+            let parts: Vec<&str> = file_name_str.split('_').collect();
+            let mut parts = parts.iter();
+
+            let mut emoji_unicode_point = String::new();
+            let _ = parts.next().unwrap();
+            let first = parts.next().unwrap().strip_prefix("u").unwrap();
+            let code_point = u32::from_str_radix(first, 16).unwrap();
+            let code_point = char::from_u32(code_point).unwrap().to_string();
+            emoji_unicode_point.push_str(&code_point);
+
+            while let Some(part) = parts.next() {
+                let code_point = u32::from_str_radix(part, 16).unwrap();
+                let code_point = char::from_u32(code_point).unwrap().to_string();
+                emoji_unicode_point.push_str(&code_point);
+            }
+
+            let img = image::open(&file_path).unwrap();
+            emojis.push(Emoji::from((img, emoji_unicode_point)));
+        }
+    }
+
+    emojis
+}
+
+pub fn debug_emojis_as_css(dir_path: PathBuf) {
+    let emojis = read_emojis_from_directory(dir_path);
+
+    let mut file = fs::File::create("debug.css").unwrap();
+    file.write(b"body {\n").unwrap();
+
+    for emoji in emojis.iter() {
+        file.write(format!("    /* {} */\n", emoji.symbol).as_bytes())
+            .unwrap();
+        file.write(
+            format!(
+                "    background-color: #{:02x?}{:02x?}{:02x?};\n",
+                emoji.color[0], emoji.color[1], emoji.color[2]
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+    }
+
+    file.write(b"}\n").unwrap();
 }
--- spritefire/src/main.rs
+++ spritefire/src/main.rs
@@ -1,3 +1,4 @@
+use spritefire::db::debug_emojis_as_css;
 use spritefire::db::EmojiDatabase;
 use std::env;
 use std::fs;
@@ -7,7 +8,7 @@ fn main() {
 
     if args[1] == "write" {
         // write db to serialized format
-        let emoji_db = EmojiDatabase::from_directory(args[2].clone().into());
+        let emoji_db = EmojiDatabase::new_from_directory(args[2].clone().into());
         let bytes = emoji_db.to_bytes();
         fs::write(args[3].clone(), bytes).unwrap();
     } else if args[1] == "read" {
@@ -15,7 +16,12 @@ fn main() {
         let bytes = fs::read(args[2].clone()).unwrap();
         let emoji_db = EmojiDatabase::from_bytes(&bytes);
         let img = image::open(args[3].clone()).unwrap();
-        let emojis = emoji_db.emojify_image_to_string(img, 7);
+        let emojis = emoji_db.emojify_image_to_string(img, 4);
         println!("{}", emojis);
+    } else if args[1] == "debug" {
+        // write average color calculated for emojis to a css file
+        // this makes it easy to view in vscode which shows color preview for
+        // the hex code
+        debug_emojis_as_css(args[2].clone().into())
     }
 }
