27df0aa37e18f82d81c0e5d4e65b46e331f05239
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2024-05-03 16:31:31 -0400

    Fix web crate build script

--- spritefire_web/build.rs
+++ spritefire_web/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let db_path = env::var("DB_PATH").unwrap_or("db.dat".to_string());
 
     if !Path::new(&db_path).exists() {
-        let assets_path = env::var("ASSETS_PATH").unwrap_or("../../assets/sprites_512".to_string());
+        let assets_path = env::var("ASSETS_PATH").unwrap_or("../assets/sprites_512".to_string());
 
         let emoji_db = EmojiDatabase::new_from_directory((&assets_path).into());
         let bytes = emoji_db.to_bytes();
